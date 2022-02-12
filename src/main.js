4a4d2fc34f5a07cdda3fe1cd22e4df9c7005cbcd
Brix-D <Shlyakhta4@gmail.com>
2022-02-12 22:34:27 +0400

    app js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+require('dotenv').config();
+import colors from 'vuetify/es5/util/colors';
+import App from './App.vue';
+import router from './router';
+
+import store from './store';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+    router,
+    store,
+    render: (h) => h(App),
+}).$mount('#app');
