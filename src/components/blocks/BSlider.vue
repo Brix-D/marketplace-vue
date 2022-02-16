ae8d626e75fb4fc962e8d7c2c48302538666c8d3
Brix-D <Shlyakhta4@gmail.com>
2022-02-17 02:18:54 +0400

    added slider

--- src/components/blocks/BSlider.vue
+++ src/components/blocks/BSlider.vue
@@ -1,16 +1,40 @@
 <template>
-    <div class="slider" />
+    <v-carousel hide-delimiter-background delimiter-icon="mdi-minus" height="150">
+        <v-carousel-item v-for="item in slides" :key="item.id">
+            <div class="slider d-flex justify-center align-center">
+                <h2 class="text-h3">{{ item.text }}</h2>
+            </div>
+        </v-carousel-item>
+    </v-carousel>
 </template>
 
 <script>
 export default {
     name: 'BSlider',
+    data() {
+        return {
+            slides: [
+                {
+                    id: 1,
+                    text: 'First slide',
+                },
+                {
+                    id: 2,
+                    text: 'Second slide',
+                },
+                {
+                    id: 3,
+                    text: 'Third slide',
+                },
+            ],
+        };
+    },
 };
 </script>
 
 <style scoped lang="scss">
 .slider {
     background-color: $color-secondary;
-    min-height: 120px;
+    min-height: 150px;
 }
 </style>
