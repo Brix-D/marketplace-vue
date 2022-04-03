76e2ba54201f17f436cbafe050f3bc1538e7f29b
Brix-D <Shlyakhta4@gmail.com>
2022-04-03 20:01:54 +0400

    added mobile menu

--- src/components/blocks/header/BMenu.vue
+++ src/components/blocks/header/BMenu.vue
@@ -1,63 +1,25 @@
 <template>
-    <v-app-bar flat :color="$vuetify.theme.currentTheme.secondaryLight" class="menu">
-        <v-container fluid class="pa-0">
-            <div class="d-flex">
-                <div class="d-flex">
-                    <v-btn
-                        href="/"
-                        tile
-                        :color="$vuetify.theme.currentTheme.secondaryLight"
-                        depressed
-                        min-height="64px"
-                        class="menu__item font-weight-regular"
-                    >
-                        Servey
-                    </v-btn>
-                </div>
-                <div class="d-flex ml-auto">
-                    <v-btn
-                        v-for="item in mainMenu"
-                        :key="item.name"
-                        :to="item.to"
-                        tile
-                        :color="$vuetify.theme.currentTheme.secondaryLight"
-                        depressed
-                        min-height="64px"
-                        class="menu__item font-weight-regular"
-                        exact
-                    >
-                        {{ item.name }}
-                    </v-btn>
-                    <template v-if="isLogged">
+    <div>
+        <v-app-bar flat :color="$vuetify.theme.currentTheme.secondaryLight" class="menu">
+            <v-container fluid class="pa-0">
+                <div v-if="!$vuetify.breakpoint.mobile" class="d-flex">
+                    <div class="d-flex">
                         <v-btn
-                            key="profile"
-                            :to="{ name: 'profileSettings' }"
+                            href="/"
                             tile
                             :color="$vuetify.theme.currentTheme.secondaryLight"
                             depressed
                             min-height="64px"
                             class="menu__item font-weight-regular"
                         >
-                            Профиль
+                            Servey
                         </v-btn>
+                    </div>
+                    <div class="d-flex ml-auto">
                         <v-btn
-                            key="logout"
-                            href="/user/logout"
-                            tile
-                            :color="$vuetify.theme.currentTheme.secondaryLight"
-                            depressed
-                            min-height="64px"
-                            class="menu__item font-weight-regular"
-                            exact
-                        >
-                            Выход
-                        </v-btn>
-                    </template>
-                    <template v-else>
-                        <v-btn
-                            v-for="item in authMenu"
+                            v-for="item in mainMenu"
                             :key="item.name"
-                            :href="item.href"
+                            :to="item.to"
                             tile
                             :color="$vuetify.theme.currentTheme.secondaryLight"
                             depressed
@@ -65,14 +27,129 @@
                             class="menu__item font-weight-regular"
                             exact
                         >
-                            <!--                        -->
                             {{ item.name }}
                         </v-btn>
-                    </template>
+                        <template v-if="isLogged">
+                            <v-btn
+                                key="profile"
+                                :to="{ name: 'profileSettings' }"
+                                tile
+                                :color="$vuetify.theme.currentTheme.secondaryLight"
+                                depressed
+                                min-height="64px"
+                                class="menu__item font-weight-regular"
+                            >
+                                Профиль
+                            </v-btn>
+                            <v-btn
+                                key="logout"
+                                href="/user/logout"
+                                tile
+                                :color="$vuetify.theme.currentTheme.secondaryLight"
+                                depressed
+                                min-height="64px"
+                                class="menu__item font-weight-regular"
+                                exact
+                            >
+                                Выход
+                            </v-btn>
+                        </template>
+                        <template v-else>
+                            <v-btn
+                                v-for="item in authMenu"
+                                :key="item.name"
+                                :href="item.href"
+                                tile
+                                :color="$vuetify.theme.currentTheme.secondaryLight"
+                                depressed
+                                min-height="64px"
+                                class="menu__item font-weight-regular"
+                                exact
+                            >
+                                <!--                        -->
+                                {{ item.name }}
+                            </v-btn>
+                        </template>
+                    </div>
                 </div>
-            </div>
-        </v-container>
-    </v-app-bar>
+                <div v-else class="d-flex justify-end">
+                    <v-btn icon @click="menuIsOpen = !menuIsOpen">
+                        <v-icon size="24">mdi-menu</v-icon>
+                    </v-btn>
+                </div>
+            </v-container>
+        </v-app-bar>
+        <v-navigation-drawer v-if="$vuetify.breakpoint.mobile" v-model="menuIsOpen" absolute app>
+            <v-list-item>
+                <v-list-item-content>
+                    <v-list-item-title
+                        class="text-h3 font-weight-regular blue--text text--darken-2"
+                    >
+                        Servey
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+            <v-divider />
+            <v-list dense>
+                <v-list-item
+                    v-for="item in mainMenu"
+                    :key="item.name"
+                    :to="item.to"
+                    exact
+                    min-height="64px"
+                >
+                    <v-list-item-content>
+                        <v-list-item-title class="subtitle-1 font-weight-regular">
+                            {{ item.name }}
+                        </v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+                <template v-if="isLogged">
+                    <v-list-item
+                        key="profile"
+                        :to="{ name: 'profileSettings' }"
+                        min-height="64px"
+                        class="font-weight-regular"
+                    >
+                        <v-list-item-content>
+                            <v-list-item-title class="subtitle-1 font-weight-regular">
+                                Профиль
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item
+                        key="logout"
+                        href="/user/logout"
+                        min-height="64px"
+                        class="font-weight-regular"
+                        exact
+                    >
+                        <v-list-item-content>
+                            <v-list-item-title class="subtitle-1 font-weight-regular">
+                                Выход
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+                <template v-else>
+                    <v-list-item
+                        v-for="item in authMenu"
+                        :key="item.name"
+                        :href="item.href"
+                        min-height="64px"
+                        class="font-weight-regular"
+                        exact
+                    >
+                        <v-list-item-content>
+                            <v-list-item-title class="subtitle-1 font-weight-regular">
+                                {{ item.name }}
+                            </v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                </template>
+            </v-list>
+        </v-navigation-drawer>
+    </div>
 </template>
 
 <script>
@@ -80,6 +157,7 @@ export default {
     name: 'BMenu',
     data() {
         return {
+            menuIsOpen: false,
             // TODO получить статус авторизации с бека
             isLogged: true,
             mainMenu: [
