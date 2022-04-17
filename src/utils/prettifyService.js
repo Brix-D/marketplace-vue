3b0d4639c21b752246b0c2357ba13d37b57860fd
Brix-D <Shlyakhta4@gmail.com>
2022-04-17 19:28:10 +0400

    parsed price

--- src/utils/prettifyService.js
+++ src/utils/prettifyService.js
@@ -5,7 +5,7 @@ export function prettifyService(service) {
         description: service.body[0].value,
         type: service.field_tip[0].value,
         category: service.field_category[0].value,
-        price: service.field_price[0].number,
+        price: parseFloat(service.field_price[0].number),
     };
 }
 
