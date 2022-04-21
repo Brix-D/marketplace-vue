b1a2c130b31ac6461520a3af82d33ed7c1965b9c
Brix-D <Shlyakhta4@gmail.com>
2022-04-21 11:04:39 +0400

    try to create proxy to auth url

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const isDev = process.env.NODE_ENV === 'development';
 
 console.log('--isDev', isDev);
+console.log('--VUE_APP_BASE_API_URI', process.env.VUE_APP_BASE_API_URI);
 module.exports = {
     lintOnSave: false,
     css: {
@@ -22,4 +23,28 @@ module.exports = {
         // },
     },
     transpileDependencies: ['vuetify'],
+    ...(isDev && {
+        devServer: {
+            host: '0.0.0.0',
+            port: 8080,
+            proxy: {
+                '/user/login': {
+                    target: process.env.VUE_APP_BASE_API_URI,
+                    secure: true,
+                    // followRedirects: true,
+                    changeOrigin: true,
+                    withCredentials: true,
+                    // cookieDomainRewrite: {
+                    //     '*': '',
+                    // },
+                    // cookiePathRewrite: {
+                    //     '*': '',
+                    // },
+                },
+            },
+            headers: {
+                'Access-Control-Allow-Origin': '*',
+            },
+        },
+    }),
 };
