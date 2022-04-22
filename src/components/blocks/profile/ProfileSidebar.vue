db60d40ee69e7b70126e1f8de3a70afea4e27277
Brix-D <Shlyakhta4@gmail.com>
2022-04-22 22:51:23 +0400

    created profile sidebar

---
+++ src/components/blocks/profile/ProfileSidebar.vue
@@ -0,0 +1,66 @@
+<template>
+    <aside>
+        <h3 class="sidebar__title">Меню</h3>
+        <v-list dense color="transparent">
+            <v-list-item
+                v-for="item in sidebarMenu"
+                :key="item.name"
+                :to="item.to"
+                exact
+                min-height="64px"
+                :ripple="false"
+                :color="$vuetify.theme.currentTheme.info"
+                class="sidebar-item"
+            >
+                <v-list-item-content>
+                    <v-list-item-title class="subtitle-1 font-weight-regular sidebar-item__title">
+                        {{ item.name }}
+                    </v-list-item-title>
+                </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </aside>
+</template>
+
+<script>
+export default {
+    name: 'ProfileSidebar',
+    data() {
+        return {
+            sidebarMenu: [
+                {
+                    name: 'Настройки',
+                    to: { name: 'profileSettings' },
+                },
+                {
+                    name: 'Финансы',
+                    to: { name: 'profileFinances' },
+                },
+                {
+                    name: 'Мои объявления',
+                    to: { name: 'collectServices' },
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.sidebar {
+    &__title {
+        font-size: 18px;
+        line-height: 1.2;
+        font-weight: 400;
+    }
+}
+.sidebar-item {
+    &::before {
+        border-radius: 8px !important;
+    }
+
+    &__title {
+        color: $color-info;
+    }
+}
+</style>
--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -1,13 +1,27 @@
 <template>
-    <div>
-        <div>profile</div>
-        <router-view />
+    <div class="">
+        <v-row dense class="profile">
+            <v-col md="10" xs="12" class="d-flex flex-column">
+                <div class="wrapper pa-3 rounded-lg d-flex flex-column flex-grow-1">
+                    <div class="profile__wrapper pa-3 d-flex flex-column flex-grow-1">
+                        <h1 class="profile__title">Личный кабинет</h1>
+                        <router-view />
+                    </div>
+                </div>
+            </v-col>
+            <v-col md="2" class="d-flex flex-column">
+                <ProfileSidebar class="wrapper pa-3 rounded-lg d-flex flex-column flex-grow-1" />
+            </v-col>
+        </v-row>
     </div>
 </template>
 
 <script>
+import ProfileSidebar from '@/components/blocks/profile/ProfileSidebar';
+
 export default {
     name: 'ProfileSettingsPage',
+    components: { ProfileSidebar },
     metaInfo() {
         return {
             title: 'Профиль | servey',
@@ -16,4 +30,16 @@ export default {
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.profile {
+    min-height: calc(100vh - 116px);
+    &__wrapper {
+        background-color: $color-white;
+    }
+    &__title {
+        font-size: 18px;
+        line-height: 1.2;
+        font-weight: 600;
+    }
+}
+</style>
