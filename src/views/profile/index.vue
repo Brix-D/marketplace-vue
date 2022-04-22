1b9fc5ed042a18dde876fe575ec745006a6b9d9c
Brix-D <Shlyakhta4@gmail.com>
2022-04-22 23:48:49 +0400

    began user profile

--- src/views/profile/index.vue
+++ src/views/profile/index.vue
@@ -4,8 +4,22 @@
             <v-col md="10" xs="12" class="d-flex flex-column">
                 <div class="wrapper pa-3 rounded-lg d-flex flex-column flex-grow-1">
                     <div class="profile__wrapper pa-3 d-flex flex-column flex-grow-1">
-                        <h1 class="profile__title">Личный кабинет</h1>
-                        <router-view />
+                        <div class="mr-15 pr-15 d-flex flex-column flex-grow-1">
+                            <h1 class="profile__title">Личный кабинет</h1>
+                            <router-view />
+                            <div class="profile__save mt-auto">
+                                <v-btn
+                                    :color="$vuetify.theme.currentTheme.info"
+                                    dark
+                                    class="rounded-lg mt-4"
+                                    depressed
+                                    large
+                                    :ripple="false"
+                                >
+                                    Сохранить
+                                </v-btn>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </v-col>
@@ -37,9 +51,12 @@ export default {
         background-color: $color-white;
     }
     &__title {
-        font-size: 18px;
+        font-size: 24px;
         line-height: 1.2;
         font-weight: 600;
+    }
+    &__save {
+        align-self: flex-end;
     }
 }
 </style>
--- src/views/profile/sections/settings.vue
+++ src/views/profile/sections/settings.vue
@@ -1,5 +1,49 @@
 <template>
-    <div>settings</div>
+    <div class="settings d-flex flex-column">
+        <h2 class="caption font-weight-medium settings__subtitle">Настройки</h2>
+        <div class="settings__role mt-4">
+            <span class="font-weight-medium">Роль пользователя:</span>
+            <span class="d-inline-block ml-4">{{ userRole }}</span>
+        </div>
+        <div class="user__main mt-4">
+            <template>
+                <v-text-field
+                    v-model="user.name"
+                    :label="user.isCompany ? 'Название компании' : 'ФИО'"
+                    outlined
+                    hide-details
+                    class="mt-5"
+                />
+                <v-text-field
+                    v-model="user.email"
+                    label="Email"
+                    outlined
+                    hide-details
+                    class="mt-5"
+                />
+                <v-text-field
+                    v-model="user.phone"
+                    label="Телефон"
+                    outlined
+                    hide-details
+                    class="mt-5"
+                />
+            </template>
+        </div>
+        <div class="user__password mt-4 d-flex flex-column align-center">
+            <div class="user__password-label font-weight-medium">Сменить пароль</div>
+            <v-btn
+                href="/user/password"
+                :color="$vuetify.theme.currentTheme.info"
+                dark
+                class="rounded-lg mt-4"
+                depressed
+                :ripple="false"
+            >
+                Сменить пароль
+            </v-btn>
+        </div>
+    </div>
 </template>
 
 <script>
@@ -10,7 +54,47 @@ export default {
             title: 'Профиль | servey',
         };
     },
+    data() {
+        return {
+            user: {
+                isCompany: false,
+                isPerson: true,
+                name: '',
+                email: '',
+                phone: '',
+            },
+        };
+    },
+    computed: {
+        userRole() {
+            let role = [];
+            if (this.user.isCompany) {
+                role.push('Компания');
+            }
+            if (this.user.isPerson) {
+                role.push('Частное лицо');
+            }
+            return role.join(',');
+        },
+    },
 };
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.settings {
+    &__subtitle {
+        font-size: 16px !important;
+    }
+    &__role {
+        font-size: 16px;
+    }
+}
+.user {
+    &__main {
+    }
+    &__password {
+    }
+    &__password-label {
+    }
+}
+</style>
