09d9d4ce5d7d6795416938a5cbf749fe5121124b
Brix-D <Shlyakhta4@gmail.com>
2022-04-25 14:55:02 +0400

    got category name from server

--- src/utils/prettifyService.js
+++ src/utils/prettifyService.js
@@ -5,7 +5,7 @@ export function prettifyService(contactId, service) {
         title: service.title[0].value || service.title,
         description: service.description || '',
         type: service.type[0].target_id === 'uslugi' ? 'Услуга' : 'Товар',
-        //category: service.field_category[0].value,
+        category: service.category,
         price: parseFloat(service.field_price[0].number),
         // author: service.author,
         // bundle: service.type[0].target_id,
