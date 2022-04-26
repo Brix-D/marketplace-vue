30df9b92749bef13d93cee58ba9c81a6e2566dc3
Brix-D <Shlyakhta4@gmail.com>
2022-04-26 20:10:03 +0400

    added filters tests

--- test/src/store/filters.spec.js
+++ test/src/store/filters.spec.js
@@ -1,5 +1,21 @@
 import { getters } from '@/store/filters';
+import catalogItems from './fixtures/catalog.json';
+import state from './fixtures/filters.json';
 
 describe('filters test', () => {
-    it('run', () => {});
+    it('should filter', () => {
+        getters.FOUND_SERVICES = jest.fn().mockImplementation((list) => list);
+        const result = getters.FILTERED_SERVICES(state, { FOUND_SERVICES: getters.FOUND_SERVICES })(
+            catalogItems
+        );
+        expect(result).toEqual([catalogItems[4]]);
+    });
+    it('should return full list', () => {
+        getters.FOUND_SERVICES = jest.fn().mockImplementation((list) => list);
+        const result = getters.FILTERED_SERVICES(
+            { items: state.items, selected: {}, search: '' },
+            { FOUND_SERVICES: getters.FOUND_SERVICES }
+        )(catalogItems);
+        expect(result).toEqual(catalogItems);
+    });
 });
